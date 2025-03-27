2afb812a28ef1b15f34281e91fa0569067c941a8
regmisan <40816347+regmisan@users.noreply.github.com>
2025-03-27 16:59:29 +0545

    Create main.tf

---
+++ templates/gcp-vm/terraform/main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
